cb50e73679f9ae8e472afa6953a5c7f5c11a674a
Nikhil Kadu <nikhil.kadu@gmail.com>
2020-01-12 14:52:20 +0530

    AWS first commit

---
+++ lambda_function/import_airline_data.py
@@ -0,0 +1,24 @@
+
+import boto3
+import csv
+import uuid
+
+s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+
+
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    csv_file_name = event['Records'][0]['s3']['object']['key']
+    print(bucket)
+    print(csv_file_name)
+    csv_object = s3_client.get_object(Bucket=bucket, Key=csv_file_name)
+    csvFileReader = csv.DictReader(csv_object['Body'].read().decode('utf-8').split('\r\n'))
+
+    table = dynamodb.Table('airlines')
+    for record in csvFileReader:
+        for key in record.keys():
+            if not record[key]: record[key] = "None"
+        record["id"] = str(uuid.uuid1())
+        print(record)
+        table.put_item(Item=record)
---
+++ lambda_function/import_employee_data.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+import csv
+import uuid
+
+s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+
+
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    json_file_name = event['Records'][0]['s3']['object']['key']
+    print(bucket)
+    print(json_file_name)
+    print(str(event))
+
+    json_object = s3_client.get_object(Bucket=bucket,Key=json_file_name)
+    jsonFileReader = json_object['Body'].read()
+    jsonDict = json.loads(jsonFileReader)
+    table = dynamodb.Table('employees')
+    for record in jsonDict:
+        print(record)
+        table.put_item(Item=record)
